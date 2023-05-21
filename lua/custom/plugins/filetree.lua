09612817d9cdb115d4ce67f32fc8334c2b35e1d0
Albert Yu <albert.yu@gmail.com>
2023-05-21 14:43:00 -0400

    Add shortcut for togling Neotree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -8,7 +8,10 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
+  init = function()
+    vim.keymap.set('n', '<leader>o', '<Cmd>Neotree toggle<CR>')
+  end,
   config = function ()
     require('neo-tree').setup {}
-  end,
+  end
 }
