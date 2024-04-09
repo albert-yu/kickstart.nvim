d3de77aee3cf392e2d2e0fa97a0666f4ebcc1b7b
Albert Yu <albert.yu@navattic.com>
2024-04-09 17:22:53 -0600

    Autorefresh files

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,2 +1,9 @@
 vim.opt.relativenumber = true
 vim.filetype.add({extension = {wgsl = "wgsl"}})
+
+-- refresh file buffer when it changes on disk
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
