a2f3b632d583a4ea5e35427da4b8020c913fe1cd
Albert Yu <albert.yu@navattic.com>
2026-04-02 10:17:04 -0400

    Use tsgo for TypeScript

--- init.lua
+++ init.lua
@@ -608,8 +608,8 @@ require('lazy').setup({
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        -- tsgo is the native TypeScript LSP from the TypeScript team
+        tsgo = {},
 
         stylua = {}, -- Used to format Lua code
 
